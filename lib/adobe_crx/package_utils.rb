05e67b8dcae8e21d1aa5c9ec58b451440a134220
Todd Tomkinson <todd.g.tomkinson@gmail.com>
2012-07-27 14:07:03 -0600

    fixed a bug where the file passed in is already a string

--- lib/adobe_crx/package_utils.rb
+++ lib/adobe_crx/package_utils.rb
@@ -3,7 +3,7 @@ require 'rexml/document'
 
 class AdobeCRX::PackageUtils
   def self.get_package_properties(package_file)
-    Zip::ZipFile.open(package_file.path, Zip::ZipFile::CREATE) do |zipfile|
+    Zip::ZipFile.open(package_file.respond_to?(:path) ? package_file.path : package_file, Zip::ZipFile::CREATE) do |zipfile|
       doc = REXML::Document.new(zipfile.read("META-INF/vault/properties.xml"))
       properties = Hash.new
       doc.elements.each("//properties/entry") do |ele|
