5089df27a4387662e9072f0317337df2e257bf9b
Todd Tomkinson <todd.g.tomkinson@gmail.com>
2012-07-30 13:07:43 -0600

    fix for an issue where opening the package was causing the zip file to be re-written, making it invalid to CRX

--- lib/adobe_crx/package_utils.rb
+++ lib/adobe_crx/package_utils.rb
@@ -3,13 +3,12 @@ require 'rexml/document'
 
 class AdobeCRX::PackageUtils
   def self.get_package_properties(package_file)
-    Zip::ZipFile.open(package_file.respond_to?(:path) ? package_file.path : package_file, Zip::ZipFile::CREATE) do |zipfile|
-      doc = REXML::Document.new(zipfile.read("META-INF/vault/properties.xml"))
-      properties = Hash.new
-      doc.elements.each("//properties/entry") do |ele|
-        properties[ele.attributes['key']] = ele.text
-      end
-      return properties
+    zipfile = Zip::ZipFile.new package_file.respond_to?(:path) ? package_file.path : package_file
+    doc = REXML::Document.new(zipfile.read("META-INF/vault/properties.xml"))
+    properties = Hash.new
+    doc.elements.each("//properties/entry") do |ele|
+      properties[ele.attributes['key']] = ele.text
     end
+    return properties
   end
 end
